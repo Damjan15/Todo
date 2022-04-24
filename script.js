bb8f71b2c653c5af5240a8c1156ebb4b14470eb6
Damjan15 <stamenovdamjan20@gmail.com>
2022-04-24 09:55:19 -0700

    Adding the data to local storage

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const form = document.getElementById("form")
 const input = document.getElementById("input")
 const todosUL = document.getElementById("todos")
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+    todos.forEach(todo => addToDo(todo));
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -23,19 +29,39 @@ function addToDo(todo) {
         }
 
         // Toggle the completed class
-        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'));
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+            updateLS();
+        });
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
 
             todoEl.remove();
+            updateLS();
         })
 
         // Adding the value & Adding it to the DOM
         todoEl.innerText = todoText;
         todosUL.appendChild(todoEl);
         input.value = "";
+
+        updateLS()
     }
+}
 
 
+function updateLS() {
+    todosEl = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
